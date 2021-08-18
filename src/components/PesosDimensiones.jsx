816c7bc12a3513198b38fbbd04b184b7b26ceefe
Luis Solano <lui.sola27@gmail.com>
2021-08-18 08:34:48 -0600

    consulta placa rtv

--- src/components/PesosDimensiones.jsx
+++ src/components/PesosDimensiones.jsx
@@ -2,16 +2,15 @@ import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import environment from '../env/environment'
 import Error from '../pages/Error'
-import Popup from 'reactjs-popup';
 
-function PesosDimensiones({  inicioSesion, usuarioIniciado}) {
+function PesosDimensiones({ inicioSesion, usuarioIniciado }) {
     const [trigger, setTrigger] = useState(false)
     const [camiones, guardarCamiones] = useState([])
     const [error, guardarError] = useState(false)
     const [consulta, setConsulta] = useState('')
 
     const apiProd = environment.url
-      
+
     const recarga = e => {
         e.preventDefault();
         setTrigger(true);
@@ -45,29 +44,18 @@ function PesosDimensiones({  inicioSesion, usuarioIniciado}) {
             <form className='mt-2' onSubmit={consulta}>
                 <div className='form-row btn-align'>
                     <div className='d-inline-flex'>
-                        <select className = 'custom-select'>
-                        <option disabled selected>Seleccionar ...</option>
-                        <option>Enero y Julio</option>
-                        <option>Febrero y Agosto</option>
-                        <option>Marzo y Septiembre</option>
-                        <option>Abril y Octubre</option>
-                        <option>Mayo y Noviembre</option>
-                        <option>Junio y Diciembre</option>
+                        <select className='custom-select'>
+                            <option disabled selected>Seleccionar ...</option>
+                            <option>Enero y Julio</option>
+                            <option>Febrero y Agosto</option>
+                            <option>Marzo y Septiembre</option>
+                            <option>Abril y Octubre</option>
+                            <option>Mayo y Noviembre</option>
+                            <option>Junio y Diciembre</option>
                         </select>
-                    <input type="submit" className="btn btn-primary ml-3 d-inline-flex" value="Consultar Pesos y Dimensiones" />
+                        <input type="submit" className="btn btn-primary ml-3 d-inline-flex" value="Consultar Pesos y Dimensiones" />
                     </div>
-                    <Popup trigger={<button type='button' className="btn btn-secondary h-75 ml-3">Actualizar</button>}
-                    modal
-                    nested>
-                    {close => (
-                        <div className="modal">
-                            <button className="close" onClick={close}>
-                                &times;
-                            </button>
-                            <span> Modal content </span>
-                        </div>
-                    )}
-                </Popup>
+                    <button type='button' className="btn btn-secondary h-75 ml-3">Actualizar</button>
                 </div>
             </form>
 
@@ -87,19 +75,19 @@ function PesosDimensiones({  inicioSesion, usuarioIniciado}) {
                             </tr>
                         </thead>
                         <tbody>
-                        {camiones.map(x =>
-                            <tr>
-                                <td>{x.placa}</td>
-                                <td>{x.unidad_negocio}</td>
-                                <td>{x.PesosDimensiones}</td>
-                            </tr>
-                        )}
-                    </tbody>
-                        </table> 
-        </div>
+                            {camiones.map(x =>
+                                <tr>
+                                    <td>{x.placa}</td>
+                                    <td>{x.unidad_negocio}</td>
+                                    <td>{x.PesosDimensiones}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             }
         </div>
     )
-    
+
 }
 export default PesosDimensiones;
